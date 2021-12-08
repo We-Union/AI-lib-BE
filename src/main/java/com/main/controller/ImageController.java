a956fd32f7d386bdb00e535a2a16e766fb76d5c3
li1553770945 <1553770945@qq.com>
2021-12-08 21:17:54 +0800

    实现图片上传下载功能

---
+++ src/main/java/com/main/controller/ImageController.java
@@ -0,0 +1,88 @@
+package com.main.controller;
+
+
+import com.main.model.User;
+import com.main.service.UserService;
+import com.main.utils.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+@RestController
+
+@RequestMapping("/image")
+public class ImageController {
+
+    @ResponseBody
+    @RequestMapping(value="/upload",produces="application/json;charset=UTF-8",method = RequestMethod.POST)
+    public  String upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws IOException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSS");
+        String res = sdf.format(new Date());
+
+        // uploads文件夹位置
+        String rootPath = request.getSession().getServletContext().getRealPath("WEB-INF/upload");
+        // 原始名称
+        String originalFileName = file.getOriginalFilename();
+        // 新文件名
+        String newFileName = UUID.randomUUID().toString().replaceAll("-","" )+ res + originalFileName.substring(originalFileName.lastIndexOf("."));
+        // 创建年月文件夹
+        Calendar date = Calendar.getInstance();
+        File dateDirs = new File(date.get(Calendar.YEAR) + File.separator + (date.get(Calendar.MONTH)+1));
+
+        // 新文件
+        File newFile = new File(rootPath + File.separator + dateDirs + File.separator + newFileName);
+        // 判断目标文件所在目录是否存在
+        if( !newFile.getParentFile().exists()) {
+            // 如果目标文件所在的目录不存在，则创建父目录
+            newFile.getParentFile().mkdirs();
+        }
+        System.out.println(newFile);
+        // 将内存中的数据写入磁盘
+        file.transferTo(newFile);
+        // 完整的url
+
+        String fileUrl = date.get(Calendar.YEAR) + "/" + (date.get(Calendar.MONTH)+1) + "/" + newFileName;
+        Map <String,String> result;
+
+        return  JsonData.buildSuccess(fileUrl);
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value="/download")
+    public void down(HttpServletRequest request, HttpServletResponse response,@RequestBody Map<String,String> map) throws Exception{
+        String file_name = map.get("file");
+        String file_path = request.getSession().getServletContext().getRealPath("WEB-INF/upload/")+file_name;
+        System.out.println(file_name);
+        System.out.println(file_path);
+        //获取输入流
+        InputStream bis = new BufferedInputStream(new FileInputStream(new File(file_path)));
+        String [] file_name_split = file_name.split("/");
+        String filename = URLEncoder.encode(file_name_split[file_name_split.length-1],"UTF-8");
+        //设置文件下载头
+        response.addHeader("Content-Disposition", "attachment;filename=" + filename);
+        response.setContentType("multipart/form-data");
+        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
+        int len = 0;
+        while((len = bis.read()) != -1){
+            out.write(len);
+            out.flush();
+        }
+        out.close();
+    }
+}
+
